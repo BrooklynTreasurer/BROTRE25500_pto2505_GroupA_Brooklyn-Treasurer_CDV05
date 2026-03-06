4b4f4949230f17af73878ceb822c816a879a346d
Brooklyn <markmclean2005@gmail.com>
2026-03-06 13:37:22 +0200

    Added the base html for header component and added a HeaderStyles.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+//React imports
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+//Component imports
+import Header from './components/ui/Header'
+
+
 function App() {
   return (
     <>
-  
+      <Header />
     </>
   )
 }
