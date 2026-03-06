500f3e0600ce8074bdb096c7feae6247a1e9982c
Brooklyn <markmclean2005@gmail.com>
2026-03-06 14:18:23 +0200

    added styling for navigation

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,21 +1,21 @@
-import HeaderStyles from './Header.css'
-
 
 export default function Header() {
     return (
         <>
-            <header className="header">
-                <h1 className="header__title">Brooklyn Treasurer</h1>
-            </header>
+          <section className="top-bar">
+                <header className="header">
+                    <h1 className="header__title">Brooklyn Treasurer</h1>
+                </header>
 
-            <nav className="nav">
-                <ul className="nav__list">
-                    <li className="nav__item">Home</li>
-                    <li className="nav__item">About</li>
-                    <li className="nav__item">Projects</li>
-                    <li className="nav__item">Contact</li>
-                </ul>
-            </nav>
+                <nav className="nav">
+                    <ul className="nav-list">
+                        <li className="nav__item">Home</li>
+                        <li className="nav__item">About</li>
+                        <li className="nav__item">Projects</li>
+                        <li className="nav__item">Contact</li>
+                    </ul>
+                </nav>
+            </section>
         </>
     )
 }
